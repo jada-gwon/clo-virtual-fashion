75aa01f4736082489a70f2d4b9c02782a054e398
jada-gwon <hyeseon.gwon@constacts.com>
2023-06-21 16:21:23 +0900

    정신건강을 위한 eslint, prettier 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,47 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
+    '@typescript-eslint/naming-convention': ['warn'],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        distinctGroup: false,
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+    'import/prefer-default-export': ['warn'],
+    'react/function-component-definition': ['off'],
     'react-refresh/only-export-components': 'warn',
   },
-}
+};

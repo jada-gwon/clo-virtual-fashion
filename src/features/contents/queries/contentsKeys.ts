ada14a117338f4ce33e2e2bdb24274f92124afe9
jada-gwon <hyeseon.gwon@constacts.com>
2023-06-23 22:20:16 +0900

    불필요한 query key 삭제

--- src/features/contents/queries/contentsKeys.ts
+++ src/features/contents/queries/contentsKeys.ts
@@ -3,9 +3,5 @@ import { ContentFilter } from '@/features/contentsFilter/types';
 const contentKeys = {
   all: ['contents'],
   list: (filter: ContentFilter) => [...contentKeys.all, 'list', filter],
-  infiniteList: (filter: ContentFilter) => [
-    ...contentKeys.list(filter),
-    'infinite',
-  ],
 };
 export default contentKeys;

5714d7fb6d3e6cb6db87527fc521418ba7dee348
jada-gwon <hyeseon.gwon@constacts.com>
2023-06-26 16:15:51 +0900

    불필요한 클래스 삭제

--- src/features/contents/components/ContentItem/index.tsx
+++ src/features/contents/components/ContentItem/index.tsx
@@ -10,8 +10,6 @@ const ContentItem: React.FC<ContentItemProps> = ({ content }) => {
     <div>
       <div>
         <img
-          // TODO: 이미지가 안떠서 임시로 추가
-          className="h-80"
           src={content.imagePath}
           alt={`${content.title} - by ${content.creator}`}
         />
